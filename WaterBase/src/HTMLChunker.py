37f8cb86d3b9e99724a90dd3e22545d385507ad6
Gabriel Høst Andersen <94848510+apathriel@users.noreply.github.com>
2024-12-04 13:56:14 +0100

    Refactor HTMLChunker class to incorporate custom HTMLStringLoader DocumentLoader, HTMLHeaderTextSplitter for splitting on headings and RecursiveCharacterTextSplitter for constraining chunk sizes.

--- WaterBase/src/HTMLChunker.py
+++ WaterBase/src/HTMLChunker.py
@@ -1,11 +1,9 @@
 import os
-from email.mime import base
-from importlib import metadata
+from typing import Dict, List, Tuple, TypedDict
 
 from bs4 import BeautifulSoup
 from DatabaseManager import CrawledLink, DatabaseManager
 from dotenv import load_dotenv
-from langchain_community.document_loaders import BSHTMLLoader
 from langchain_core.document_loaders import BaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import (
@@ -14,14 +12,60 @@ from langchain_text_splitters import (
 )
 
 
-class HTMLChunker:
-    def __init__(self, db_url: str):
-        self.db_manager = DatabaseManager(connection_string=db_url)
+class ChunkingConfigurations(TypedDict):
+    chunk_size: int
+    chunk_overlap: int
 
-    def proto(self):
+
+class HTMLChunker:
+    def __init__(
+        self,
+        db_url: str,
+        chunking_configurations: ChunkingConfigurations,
+        headers_to_split_on: List[Tuple[str, str]] = None,
+    ):
+        self.db_manager = DatabaseManager(connection_string=db_url)
+        self.documents: List[CrawledLink] = []
+        self.headers_to_split_on = headers_to_split_on or [
+            ("h1", "Header 1"),
+            ("h2", "Header 2"),
+            ("h3", "Header 3"),
+        ]
+        self.splitter: HTMLHeaderTextSplitter = HTMLHeaderTextSplitter(
+            self.headers_to_split_on, return_each_element=True
+        )
+        self.loader: HTMLStringLoader = None
+        self.recursive_splitter: RecursiveCharacterTextSplitter = (
+            RecursiveCharacterTextSplitter(
+                chunk_size=chunking_configurations["chunk_size"],
+                chunk_overlap=chunking_configurations["chunk_overlap"],
+            )
+        )
+
+    def fetch_documents(self):
         return self.db_manager.fetch_links_with_content(
             CrawledLink, limit=1, filters=[CrawledLink.allowed == True]
         )
+
+    def load_document(self, sql_item: CrawledLink) -> Document:
+        self.loader = HTMLStringLoader(sql_item)
+        return self.loader.load()
+
+    def run(self):
+        self.documents = self.fetch_documents()
+        for doc in self.documents:
+            data = self.load_document(doc)
+            # Split the document using HTMLHeaderTextSplitter
+            header_chunks = self.splitter.split_text(data.page_content)
+            # Append metadata to each chunk
+            for chunk in header_chunks:
+                chunk.metadata["source"] = data.metadata["source"]
+                chunk.metadata["title"] = data.metadata["title"]
+            # Split the chunks using RecursiveCharacterTextSplitter
+            splits = self.recursive_splitter.split_documents(header_chunks)
+            print(type(splits[0]))
+            print(f"Initial header split count: {len(header_chunks)}")
+            print(f"Final split count: {len(splits)}")
 
 
 class HTMLStringLoader(BaseLoader):
@@ -38,34 +82,11 @@ class HTMLStringLoader(BaseLoader):
 
 
 def main():
-    headers_to_split_on = [
-        ("h1", "Header 1"),
-        ("h2", "Header 2"),
-        ("h3", "Header 3"),
-    ]
     load_dotenv()
     db_url = os.getenv("SQLALCHEMY_DATABASE_URL")
-    chunker = HTMLChunker(db_url)
-    html_splitter = HTMLHeaderTextSplitter(
-        headers_to_split_on, return_each_element=True
-    )
-    split = chunker.proto()
-    custom_loader = HTMLStringLoader(split[0])
-    data = custom_loader.load()
-    chunks = html_splitter.split_text(data.page_content)
-    recursive_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=100, chunk_overlap=20, length_function=len, is_separator_regex=False
-    )
-    bs_loader = BSHTMLLoader("out/cases/1565.html")
-    docs = bs_loader.load()
-    custom_texts = recursive_splitter.split_documents([data])
-    base_texts = recursive_splitter.split_documents(docs)
-
-    print(len(custom_texts))
-    print(len(base_texts))
-
-    print(custom_texts[0])
-    print(base_texts[0])
+    chunking_config = ChunkingConfigurations(chunk_size=250, chunk_overlap=25)
+    chunker = HTMLChunker(db_url, chunking_config)
+    chunker.run()
 
 
 if __name__ == "__main__":
